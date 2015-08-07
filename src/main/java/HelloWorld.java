d86bbfff5af8ee4ff7f65f0fae035c9cd43266fa
Oliver Kopp <kopp.dev@gmail.com>
2015-08-07 19:26:02 +0200

    Add gradle build script

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Hello World Java class to demonstrate the usage of the sonar plugin
+ *
+ * Please delete this file in your repository
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+
+}
